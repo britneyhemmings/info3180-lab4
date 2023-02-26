7a6aa4fa7c16038a39ee2bff7eefa8c3f47bc8b8
Britney Hemmings <britneyhemmings1@gmail.com>
2023-02-26 15:30:43 -0500

    updated the login route

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -43,8 +44,9 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
-        # Get the username and password values from the form.
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data# Get the username and password values from the form.
 
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
@@ -52,11 +54,24 @@ def login():
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
 
-        # Gets user id, load into session
-        login_user(user)
+        user = db.session.execute(db.select(UserProfile).filter_by(username=username)).scalar()
 
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
+        if user is not None and check_password_hash(user.password, password):
+            remember_me = False
+
+            if 'remember_me' in request.form:
+                remember_me = True
+
+            # Gets user id, load into session
+            login_user(user, remember = remember_me)
+
+            # Remember to flash a message to the user
+            flash('Logged in successfully.', 'success')
+
+            return redirect(url_for("upload"))  # The user should be redirected to the upload form instead
+        else:
+            flash('Username or Password is incorrect.', 'danger')
+
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
